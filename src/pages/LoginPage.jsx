fc1603e3182f931af8b0b7c908346a3e618bb64c
Heavensliving <heavensliving@gmail.com>
2024-08-16 18:43:26 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,7 +9,7 @@ const LoginPage = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/user/login', {
+      const response = await fetch('https://heavensmanagement.onrender.com/api/user/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
